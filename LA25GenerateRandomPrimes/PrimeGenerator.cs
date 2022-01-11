cbfa1f299202c86bc822d62f5623c44fc3399efc
Nicole W <96558445+CodeLearnr@users.noreply.github.com>
2022-01-10 21:31:08 -0700

    Add files via upload

---
+++ LA25GenerateRandomPrimes/PrimeGenerator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LA25GenerateRandomPrimes
+{
+    class PrimeGenerator
+    {
+        public void GenPrimeNo() //Print 1 random prime
+        {
+            bool isPrime = false;
+            int x;
+            do
+            {
+                Random randA = new Random();
+                x = randA.Next(0,10000);
+                if (x <= 1)
+                {
+                    isPrime = false;
+                }
+                for (int i = 2; i*i < x; i++)
+                {
+                    if (x % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                    for(int j =2; j*j<x; j++)
+                    {
+                        if (x % i != 0)
+                        isPrime = true;
+                    }
+                }
+                
+            } while (isPrime == false);
+            Console.Write(x + " ");
+        }
+        public void GenPrimeNo(int n) //Print n random primes
+        {
+            for (int k = 0; k < n; k++)
+            {
+                //Thread.Sleep(100);
+                GenPrimeNo();
+            }
+        }
+        public void GenPrimeNo(int a, int b) // a<=b
+        {
+            for (int i = a; i <= b; i++)
+            {
+                int divisor = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            divisor = 1;
+                            break;
+                        }
+                    }
+                    if (divisor == 0)
+                    {
+                        Console.Write(i + " ");
+                    }
+                }
+            }
+        }
+        public void GenPrimeNo(int c, int a, int b) // c>=1, a<=b, print 1st c primes in range (a,b)
+        {
+            int count = 0;
+            for (int i = a; i <= b; i++)
+            {
+                int divisor = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            divisor = 1;
+                            break;
+                        }
+                    }
+                    if (divisor == 0 && count < c)
+                    {
+                        Console.Write(i + " ");
+                        count = count + 1;
+
+                    }
+                }
+            }
+            if (count < c)
+            {
+                Console.WriteLine("\n!!!There are only {0} primes in the range you have selected.!!! \nPlease select a bigger range", count);
+            }
+
+        }
+    }
+}
---
+++ LA25GenerateRandomPrimes/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace LA25GenerateRandomPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==== LA 25 Generate Random Primes (Method overrides)====\n");
+            PrimeGenerator primeAB1 = new PrimeGenerator();
+
+            Console.Write("Overloaded Method 1: No parameter. \nA generated random prime: ");
+            primeAB1.GenPrimeNo();
+            Console.WriteLine("\n=======================================");
+
+            Console.Write("\nOverloaded Method 2: 1 parameter. " +
+                "\nInput how many random primes do you want to display?: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("The {0} generated random primes: ", n);
+            primeAB1.GenPrimeNo(n);
+            Console.WriteLine("\n=======================================");
+
+            Console.Write("\nOverloaded Method 3: 2 parameters. \nInput a lower range for random primes: ");
+            int lowerR = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input an upper range: ");
+            int upperR = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("All primes in the range: {0} to {1}:", lowerR, upperR);
+            primeAB1.GenPrimeNo(lowerR, upperR);
+            Console.WriteLine("\n=============================================");
+
+            Console.Write("\nOverloaded Method 4: 3 parameters. \nHow many primes do you want to display?: ");
+            int noOfPrime = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input a lower range: ");
+            int lowerR1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input an upper range: ");
+            int upperR1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The first {0} primes in the range: {1} to {2}:",noOfPrime, lowerR1, upperR1);
+            primeAB1.GenPrimeNo(noOfPrime,lowerR1,upperR1);
+
+            //int a = primeAB1.// override object.Equals
+    
+
+            Console.WriteLine("\n\n=================== Press Any Key to Exit ===================");
+            
+            
+            Console.ReadLine();
+        }
+        /*Create a classes with overloaded methods to represent a Prime Generator.
+        1. Create a method to determine if a number is a prime, and four overloaded methods to generate primes.
+        2. Method without a parameter should generate and display a random prime, method with one integer parameter n will generate and display n random primes, method with two integer parameters (a, b; a<=b) will generate all primes in the range(a, b), and method with three integer parameters(c, a, b; c>=1, a<=b) will generate and display the first c primes(if exists) in the range(a, b).
+        3. Complete all the incomplete methods from the class. And do not alter the Program class.
+        Sample Input/Output:
+        prime
+
+        Random Prime: 61
+        prime 5 5
+
+        Random Primes: 787 863 659 277 739
+        prime 10  20
+
+        All primes in the range 10 to 20: 11 13 17 19
+        prime 5 100 200 First 5 Primes in range 100 to 200: 101 103 107 109 113*/
+    }
+}
